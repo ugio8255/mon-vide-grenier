5411042ae54e1b6bd5b53fcbe331f58143f96197
usai giovanni <giovanni@iMac-de-usai.local>
2026-05-05 14:42:07 +0200

    Recréation route API

--- app/lib/produits.ts
+++ app/lib/produits.ts
@@ -1,19 +1,14 @@
 export interface Produit {
-  id: number;
+  id: string;
   nom: string;
   categorie: string;
   prix: number;
   etat: string;
-  image?: string
+  image: string;
+  quantite: number;
 }
 
-const PRODUITS_INITIAUX: Produit[] = [
-  { id: 1, nom: "Vase décoratif en verre soufflé bleu", categorie: "DÉCORATION", prix: 45, etat: "Très bon état" },
-  { id: 2, nom: "Montre vintage Invicta", categorie: "MÉDIA & TECH", prix: 150, etat: "Très bon état" },
-  { id: 3, nom: "Trépied photo Amazon Basics", categorie: "MÉDIA & TECH", prix: 50, etat: "Très bon état" },
-  { id: 4, nom: "Livre ancien relié cuir", categorie: "COLLECTION / VINTAGE", prix: 50, etat: "Dans son jus" },
-  { id: 5, nom: "Petite figurine d'éléphant rose", categorie: "DÉCORATION", prix: 10, etat: "Très bon état" },
-];
+const PRODUITS_INITIAUX: Produit[] = [];
 
 export function getProduits(): Produit[] {
   if (typeof window === "undefined") return PRODUITS_INITIAUX;
