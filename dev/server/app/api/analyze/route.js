c3c8ca88984f2cf9282705f4a3e6aab14bf51051
usai giovanni <giovanni@iMac-de-usai.local>
2026-05-11 15:45:47 +0200

    Suppression dossiers parasites

--- dev/server/app/api/analyze/route.js
+++
@@ -1,7 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/analyze/route.js")
-R.c("server/chunks/app_api_analyze_route_ts_0yr85w9._.js")
-R.c("server/chunks/node_modules_next_0.ww9hi._.js")
-R.c("server/chunks/[root-of-the-server]__0ml1c3u._.js")
-R.c("server/chunks/_next-internal_server_app_api_analyze_route_actions_0ci5evp.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/analyze/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/analyze/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
